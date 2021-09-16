4510215c906b8101195b0c557cf4f1a63d5e6b1d
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-16 23:17:24 +0530

    Update main.js

--- D/main.js
+++ D/main.js
@@ -3,6 +3,7 @@ const main = document.getElementById("main");
 for(var i=0; i<10; i++){
     var head = document.createElement('h4');
     head.innerHTML = `my name is sonu`;
+    main.appendChild(head);
 }
 
-main.appendChild(head);
+
